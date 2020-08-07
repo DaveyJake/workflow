1fd78e39a1e97f24cacd44a120b84aedabe3790d
Davey Jacobson <daveyjake21@gmail.com>
2020-08-07 13:38:11 -0600

    Create template-tags.php
    Custom template tags that are used inside `{page|single|content}-{slug|$post_type}.php` files.

---
+++ inc/template-tags.php
@@ -0,0 +1,280 @@
+<?php
+/**
+ * Custom template tags that are used inside {page|single|content}-{slug|$post_type}.php files.
+ *
+ * Eventually, some of the functionality here could be replaced by core features.
+ *
+ * @package Project
+ */
+
+if ( ! defined( 'ABSPATH' ) ) exit; // phpcs:ignore
+
+if ( ! function_exists( 'project_body_open' ) ) :
+	/**
+	 * Shim for sites older than 5.2.
+	 *
+	 * @link https://core.trac.wordpress.org/ticket/12563
+	 */
+	function project_body_open() {
+		do_action( 'project_body_open' );
+	}
+endif;
+
+if ( ! function_exists( 'project_body_close' ) ) :
+	/**
+	 * Shim for sites older than 5.2.
+	 *
+	 * @link https://core.trac.wordpress.org/ticket/12563
+	 */
+	function project_body_close() {
+		do_action( 'project_body_close' );
+	}
+endif;
+
+if ( ! function_exists( 'project_custom_logo' ) ) :
+	/**
+	 * Output the custom logo HTML.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @global int $blog_id Default: 1, except on subsites.
+	 *
+	 * @param bool $echo True outputs HTML. False returns HTML.
+	 */
+	function project_custom_logo( $echo = true ) {
+		global $blog_id;
+
+		$html  = '<span class="ssf-logo site-logo__network">';
+		$html .= sprintf( '<span class="ssf-logo__content">%s</span><span class="ssf-logo__content">%s</span><span class="ssf-logo__content">%s</span>', 'Project Smith', '& Frampton', 'Real Estate' );
+		$html .= '</span>';
+
+		if ( 1 !== $blog_id ) {
+			$html .= '<span class="site-logo__agent">';
+			$html .= sprintf( '<span class="site-logo__agent-content">%s</span><span class="site-logo__agent-content">%s <span>%s</span></span><span class="site-logo__agent-content">%s</span>', 'THE', 'LOCALS\'', 'guide', 'vail valley' );
+			$html .= '</span>';
+		}
+
+		if ( $echo ) {
+			echo wp_kses_post( $html );
+		} else {
+			return $html;
+		}
+	}
+endif;
+
+if ( ! function_exists( 'project_entry_footer' ) ) :
+	/**
+	 * Prints HTML with meta information for the categories, tags and comments.
+	 */
+	function project_entry_footer() {
+		// Hide category and tag text for pages.
+		if ( 'post' === get_post_type() ) {
+			/* translators: used between list items, there is a space after the comma */
+			$categories_list = get_the_category_list( esc_html__( ', ', 'project' ) );
+			if ( $categories_list ) {
+				/* translators: 1: list of categories. */
+				printf( '<span class="cat-links">' . esc_html__( 'Posted in %1$s', 'project' ) . '</span>', $categories_list ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+			}
+
+			/* translators: used between list items, there is a space after the comma */
+			$tags_list = get_the_tag_list( '', esc_html_x( ', ', 'list item separator', 'project' ) );
+			if ( $tags_list ) {
+				/* translators: 1: list of tags. */
+				printf( '<span class="tags-links">' . esc_html__( 'Tagged %1$s', 'project' ) . '</span>', $tags_list ); // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+			}
+		}
+
+		if ( ! is_single() && ! post_password_required() && ( comments_open() || get_comments_number() ) ) {
+			echo '<span class="comments-link">';
+			comments_popup_link(
+				sprintf(
+					wp_kses(
+						/* translators: %s: post title */
+						__( 'Leave a Comment<span class="screen-reader-text"> on %s</span>', 'project' ),
+						array(
+							'span' => array(
+								'class' => array(),
+							),
+						)
+					),
+					wp_kses_post( get_the_title() )
+				)
+			);
+			echo '</span>';
+		}
+
+		edit_post_link(
+			sprintf(
+				wp_kses(
+					/* translators: %s: Name of current post. Only visible to screen readers */
+					__( 'Edit <span class="screen-reader-text">%s</span>', 'project' ),
+					array(
+						'span' => array(
+							'class' => array(),
+						),
+					)
+				),
+				wp_kses_post( get_the_title() )
+			),
+			'<span class="edit-link">',
+			'</span>'
+		);
+	}
+endif;
+
+if ( ! function_exists( 'project_post_thumbnail' ) ) :
+	/**
+	 * Displays an optional post thumbnail.
+	 *
+	 * Wraps the post thumbnail in an anchor element on index views, or a div
+	 * element when on single views.
+	 */
+	function project_post_thumbnail() {
+		if ( post_password_required() || is_attachment() || ! has_post_thumbnail() ) {
+			return;
+		}
+
+		if ( is_singular() ) :
+			?>
+			<div class="post-thumbnail">
+				<?php the_post_thumbnail(); ?>
+			</div><!-- .post-thumbnail -->
+			<?php
+		else :
+			?>
+			<a class="post-thumbnail" href="<?php the_permalink(); ?>" aria-hidden="true" tabindex="-1">
+				<?php
+					the_post_thumbnail(
+						'post-thumbnail',
+						array(
+							'alt' => the_title_attribute(
+								array(
+									'echo' => false,
+								)
+							),
+						)
+					);
+				?>
+			</a>
+			<?php
+		endif;
+	}
+endif;
+
+if ( ! function_exists( 'project_posted_by' ) ) :
+	/**
+	 * Prints HTML with meta information for the current author.
+	 */
+	function project_posted_by() {
+		$byline = sprintf(
+			/* translators: %s: post author. */
+			esc_html_x( 'by %s', 'post author', 'project' ),
+			'<span class="author vcard"><a class="url fn n" href="' . esc_url( get_author_posts_url( get_the_author_meta( 'ID' ) ) ) . '">' . esc_html( get_the_author() ) . '</a></span>'
+		);
+
+		echo '<span class="byline"> ' . $byline . '</span>'; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+
+	}
+endif;
+
+if ( ! function_exists( 'project_posted_on' ) ) :
+	/**
+	 * Prints HTML with meta information for the current post-date/time.
+	 */
+	function project_posted_on() {
+		$time_string = '<time class="entry-date published updated" datetime="%1$s">%2$s</time>';
+		if ( get_the_time( 'U' ) !== get_the_modified_time( 'U' ) ) {
+			$time_string = '<time class="entry-date published" datetime="%1$s">%2$s</time><time class="updated" datetime="%3$s">%4$s</time>';
+		}
+
+		$time_string = sprintf(
+			$time_string,
+			esc_attr( get_the_date( DATE_W3C ) ),
+			esc_html( get_the_date() ),
+			esc_attr( get_the_modified_date( DATE_W3C ) ),
+			esc_html( get_the_modified_date() )
+		);
+
+		$posted_on = sprintf(
+			/* translators: %s: post date. */
+			esc_html_x( 'Posted on %s', 'post date', 'project' ),
+			'<a href="' . esc_url( get_permalink() ) . '" rel="bookmark">' . $time_string . '</a>'
+		);
+
+		echo '<span class="posted-on">' . $posted_on . '</span>'; // phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+
+	}
+endif;
+
+if ( ! function_exists( 'project_property_search' ) ) :
+	/**
+	 * Prints HTML for the property search filters.
+	 */
+	function project_property_search() {
+		$location_args = array(
+			'id'          => 'property-search-location',
+			'placeholder' => 'CITY, ZIP, ADDRESS, or MLS#',
+			'type'        => 'text',
+			'icon'        => 'search',
+			'echo'        => true,
+		);
+
+		$min_price_args = array(
+			'id'          => 'property-min-price',
+			'placeholder' => 'MIN PRICE',
+			'echo'        => true,
+		);
+
+		$max_price_args = array(
+			'id'          => 'property-max-price',
+			'placeholder' => 'MAX PRICE',
+			'echo'        => true,
+		);
+
+		$bed_args = array(
+			'id'          => 'property-beds',
+			'placeholder' => 'BEDS',
+			'echo'        => true,
+		);
+
+		$bath_args = array(
+			'id'          => 'property-baths',
+			'placeholder' => 'BATHS',
+			'echo'        => true,
+		);
+
+		$property_type_args = array(
+			'id'          => 'property-type',
+			'placeholder' => 'TYPE',
+			'echo'        => true,
+		);
+
+		echo '<div class="property-search">';
+
+			echo '<span class="property-search__input property-search__input-location">';
+				project_input_box( $location_args );
+			echo '</span>';
+
+			echo '<span class="property-search__input property-search__input-min-price">';
+				project_dropdown_menu( $min_price_args );
+			echo '</span>';
+
+			echo '<span class="property-search__input property-search__input-max-price">';
+				project_dropdown_menu( $max_price_args );
+			echo '</span>';
+
+			echo '<span class="property-search__input property-search__input-beds">';
+				project_dropdown_menu( $bed_args );
+			echo '</span>';
+
+			echo '<span class="property-search__input property-search__input-baths">';
+				project_dropdown_menu( $bath_args );
+			echo '</span>';
+
+			echo '<span class="property-search__input property-search__input-property-type">';
+				project_dropdown_menu( $property_type_args );
+			echo '</span>';
+
+		echo '</div>';
+	}
+endif;
